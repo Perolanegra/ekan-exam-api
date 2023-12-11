700820f169f361258d68d1d585f4b5e5288fdb96
perolanegra <pedratto3@gmail.com>
2023-12-11 16:28:51 +0000

    remove useless imports

--- src/main/java/br/com/ekan/repository/DocumentRepository.java
+++ src/main/java/br/com/ekan/repository/DocumentRepository.java
@@ -1,15 +1,11 @@
 package br.com.ekan.repository;
 
-import br.com.ekan.model.Beneficiary;
 import br.com.ekan.model.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface DocumentRepository extends JpaRepository<Document, Long> {
-
 
     Document findByDocumentTypeAndDesc(String documentType, String desc);
 }
